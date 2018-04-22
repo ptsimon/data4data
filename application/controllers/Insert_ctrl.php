7cad1f13f408dfdc9f62368e1a817fcd117a651c
ptsimon <simon.paolafaith@gmail.com>
2018-04-23 02:37:04 +0800

    fixed?

--- application/controllers/Insert_ctrl.php
+++ application/controllers/Insert_ctrl.php
@@ -13,22 +13,22 @@ class Insert_ctrl extends CI_Controller {
 		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 
 		//Validating Student Number Field
-		$this->form_validation->set_rules('dsnum', 'Student Number', 'required|min_length[9]|max_length[10]');
+		$this->form_validation->set_rules('dsnum', 'Student No', 'required|min_length[9]|max_length[10]');
 
 		//Validating Username Field
-		$this->form_validation->set_rules('dsnum', 'Username', 'required|min_length[5]|max_length[15]');
+		$this->form_validation->set_rules('duname', 'Username', 'required|min_length[5]|max_length[25]');
 
 		//Validating Password Field
-		$this->form_validation->set_rules('dpass', 'Password', 'required|');
+		$this->form_validation->set_rules('dpass', 'Password', 'required|min_length[8]|max_length[25]');
 
 		if ($this->form_validation->run() == FALSE) {
 		$this->load->view('insert_view');
 		} else {
 			//Setting values for tabel columns
 			$data = array(
-			'Student_Number' => $this->input->post('dsnum'),
-			'Username' => $this->input->post('duname'),
-			'Password' => $this->input->post('dpass'),
+				'student_num' => $this->input->post('dsnum'),
+				'username' => $this->input->post('duname'),
+				'password' => $this->input->post('dpass'),
 			);
 			//Transfering data to Model
 			$this->insert_model->form_insert($data);
--- application/models/Insert_model.php
+++ application/models/Insert_model.php
@@ -1,13 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Insert_model extends CI_Model{
-	
+
 	public function __construct() {
 		parent::__construct();
 	}
 	function form_insert($data){
 		// Inserting in Table(students) of Database(college)
-		// $this->db->insert('students', $data);
+		$this->db->insert('registration', $data);
 	}
 }
 
