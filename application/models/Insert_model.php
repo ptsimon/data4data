30d890a63c576f8d44f8f8ebe3f84bb0c5d1473c
Paola Faith Simon <simon.paolafaith@gmail.com>
2018-04-24 11:16:56 +0800

    debug

--- application/models/Insert_model.php
+++ application/models/Insert_model.php
@@ -2,23 +2,6 @@
 
 class Insert_model extends CI_Model{
 
-	// public function __construct() {
-	// 	parent::__construct();
-	// }
-	// function form_insert($data){
-	// 	// Inserting in Table(students) of Database(college)
-	// 	// $this->load->database();
-	// 	$query = $this->db->insert('reg2', $data);
-	// 	if($query)
-	// 	  {
-	// 	    return true;  
-	// 	  }
-	// 	  else
-	// 	  {
-	// 	    return false; 
-	// 	  }
-	// }
-
 	function __construct()
     {
         parent::__construct();
@@ -30,19 +13,18 @@ class Insert_model extends CI_Model{
             'student_num' => $this->input->post('dsnum'),
             'username' => $this->input->post('duname'),
             'password' => $this->input->post('dpass')
-            // 'student_num' => 'yes',
-            // 'username' => 'yes',
-            // 'password' => 'yes'
         );
-        foreach ($data as $key => $value) {
-        	echo $key;
-        	echo $value;
-        }
-
+        
         $this->load->database();
         // users is the name of the db table you are inserting in
-        return $this->db->insert('reg2', $data);
-    }   
-}
+        $query = return $this->db->insert('reg2', $data);
+		if($query) {
+		    return true;  
+		} else {
+		    return false; 
+		}
+	}    
+}   
+
 
 ?>
