ca81bb8a897066fe4cedd1809f41d1226042d7d4
Paola Faith Simon <simon.paolafaith@gmail.com>
2018-04-24 11:03:55 +0800

    debug

--- application/controllers/Insert_ctrl.php
+++ application/controllers/Insert_ctrl.php
@@ -2,51 +2,73 @@
 
 class Insert_ctrl extends CI_Controller {
 
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('Insert_model');
-	}
-	function index() {
-		//Including validation library
-		$this->load->library('form_validation');
+	// function __construct() {
+		// parent::__construct();
+		// $this->load->model('Insert_model');
+	// }
+	// function index() {
+	// 	//Including validation library
+	// 	$this->load->library('form_validation');
+	// 	$this->load->helper(array('form', 'url'));
 
-		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+	// 	$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
 
-		//Validating Student Number Field
-		$this->form_validation->set_rules('dsnum', 'Student No', 'required|min_length[9]|max_length[10]');
+	// 	//Validating Student Number Field
+	// 	$this->form_validation->set_rules('dsnum', 'Student No', 'required|min_length[9]|max_length[10]');
 
-		//Validating Username Field
-		$this->form_validation->set_rules('duname', 'Username', 'required|min_length[5]|max_length[25]');
+	// 	//Validating Username Field
+	// 	$this->form_validation->set_rules('duname', 'Username', 'required|min_length[5]|max_length[25]');
 
-		//Validating Password Field
-		$this->form_validation->set_rules('dpass', 'Password', 'required|min_length[8]|max_length[25]');
+	// 	//Validating Password Field
+	// 	$this->form_validation->set_rules('dpass', 'Password', 'required|min_length[8]|max_length[25]');
 
-		if ($this->form_validation->run() == FALSE) {
-			$this->load->view('insert_view');
+	// 	if ($this->form_validation->run() == FALSE) {
+	// 		$this->load->view('insert_view');
 
-		} else {
-			//Setting values for tabel columns
-			// $this->load->library('database');
-			// echo "hello";
-			$data = array(
-				// 'id' => NULL,
-				// 'ts' => NULL,
-				'student_num' => $this->input->post('dsnum'),
-				'username' => $this->input->post('duname'),
-				'password' => $this->input->post('dpass'),
-			);
-			//Transfering data to Model
-			$this->load->view('insert_view', $data);
-			$success = $this->Insert_model->form_insert($data);
-			if($success == true) {
-				$data['message'] = 'Data Inserted Successfully';
-			} else {
-				$data['message'] = 'Data not inserted successfully';
-			}
-			//Loading View
-			$this->load->view('insert_view', $data);
-		}
-	}
+	// 	} else {
+	// 		//Setting values for tabel columns
+	// 		// $this->load->library('database');
+	// 		// echo "hello";
+	// 		$data = array(
+	// 			// 'id' => NULL,
+	// 			// 'ts' => NULL,
+	// 			'student_num' => $this->input->post('dsnum'),
+	// 			'username' => $this->input->post('duname'),
+	// 			'password' => $this->input->post('dpass'),
+	// 		);
+	// 		//Transfering data to Model
+	// 		$success = $this->Insert_model->form_insert($data);
+	// 		if($success == true) {
+	// 			$data['message'] = 'Data Inserted Successfully';
+	// 		} else {
+	// 			$data['message'] = 'Data not inserted successfully';
+	// 		}
+	// 		//Loading View
+	// 		$this->load->view('insert_view', $data);
+	// 	}
+	// }
+
+	public function index()
+            {
+        $this->load->helper(array('form', 'url'));
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('dsnum', 'Student No', 'required|min_length[9]|max_length[10]');
+        $this->form_validation->set_rules('duname', 'Username', 'required|min_length[5]|max_length[25]');
+        $this->form_validation->set_rules('dpass', 'Password', 'required|min_length[8]|max_length[25]');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->view('insert_view');
+        }
+        else
+        {
+            $this->load->model('Insert_model');
+            $this->Insert_model->insert_user();
+            $data['message'] = 'Data Inserted Successfully';
+            $this->load->view('insert_view', $data);
+        }
+    }
 
 }
 
--- application/models/Insert_model.php
+++ application/models/Insert_model.php
@@ -2,22 +2,44 @@
 
 class Insert_model extends CI_Model{
 
-	public function __construct() {
-		parent::__construct();
-	}
-	function form_insert($data){
-		// Inserting in Table(students) of Database(college)
-		// $this->load->database();
-		$query = $this->db->insert('reg2', $data);
-		if($query)
-		  {
-		    return true;  
-		  }
-		  else
-		  {
-		    return false; 
-		  }
-	}
+	// public function __construct() {
+	// 	parent::__construct();
+	// }
+	// function form_insert($data){
+	// 	// Inserting in Table(students) of Database(college)
+	// 	// $this->load->database();
+	// 	$query = $this->db->insert('reg2', $data);
+	// 	if($query)
+	// 	  {
+	// 	    return true;  
+	// 	  }
+	// 	  else
+	// 	  {
+	// 	    return false; 
+	// 	  }
+	// }
+
+	function __construct()
+    {
+        parent::__construct();
+    }
+
+	    public function insert_user()
+    {    
+        $data = array(
+            // 'student_num' => $this->input->post('dsnum'),
+            // 'username' => $this->input->post('duname'),
+            // 'password' => $this->input->post('dpass')
+            'student_num' => 'yes',
+            'username' => 'yes',
+            'password' => 'yes'
+        );
+        echo $data
+
+        $this->load->database();
+        // users is the name of the db table you are inserting in
+        return $this->db->insert('reg2', $data);
+    }   
 }
 
 ?>
